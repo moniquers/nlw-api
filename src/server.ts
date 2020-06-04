0fdbe1c62573acb243f6856e171ea706ca96d4be
Monique Ribeiro de Sousa <ribeiromonique90@gmail.com>
2020-06-04 19:43:07 -0300

    configura rotas e banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/Users', (request, response) => {
-
-    response.json(
-        [
-            "Monique",
-            "Evandro"
-        ]
-    )
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
