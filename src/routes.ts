ed7e68033847bce06eb9e8b0f73e2465abdb9d57
Monique Ribeiro de Sousa <ribeiromonique90@gmail.com>
2020-06-04 23:10:30 -0300

    implemeta endpoints diversos de inserção e consulta de dados dos pontos de coleta e itens com uso de Controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { request, response } from 'express';
+
+import PointsController from "./Controllers/PointsController";
+import ItemsController from "./Controllers/ItemsController";
 
 const routes = express.Router();
+const pointsController = new PointsController;
+const itemsController = new ItemsController;
 
-routes.get('/', (request, response) => {
-    response.json({message: "Hello World"})
-});
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
